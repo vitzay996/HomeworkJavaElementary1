97c07191e2d5206bdab8dec39fb5dd84494ae7f2
Viktor Zaiets <vitzay96@gmail.com>
2023-03-07 19:05:30 +0200

    create InterfaceTask and Task, change code

--- src/com/vitzay/Task.java
+++ src/com/vitzay/Task.java
@@ -1,4 +1,22 @@
 package com.vitzay;
 public class Task {
+    public String task;
+    protected String state = "uncompleted";
 
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return task + ", " + state;
+    }
 }
--- src/com/vitzay/TodoList.java
+++ src/com/vitzay/TodoList.java
@@ -1,7 +1,22 @@
 package com.vitzay;
 
+import java.util.Scanner;
+
 public class TodoList {
+    public static Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
 
+    }
+
+    public static void actionSetFromTheMenu() {
+        int numberOperation = readNumberOperation();
+
+    }
+
+    public static int readNumberOperation() {
+        System.out.println("Please enter number desired operation");
+        int numberOperation = SCANNER.nextInt();
+        String str1 = SCANNER.nextLine(); //scanner bug fix
+        return numberOperation;
     }
 }
